2197e644ec916a2aca79f870604cfb6ca7925f58
Ingrid Knochenhauer de Souza <50635626+ingridkno@users.noreply.github.com>
2022-01-10 18:22:01 -0300

    Update app_sinapi.py

--- app_sinapi.py
+++ app_sinapi.py
@@ -5,19 +5,31 @@ import streamlit as st
 st.set_page_config(layout="wide")
 
 
-file = 'SINAPI_Custo_Ref_Composicoes_Analitico_SC_202110_Desonerado.csv'
+file = 'SINAPI_Custo_Ref_Composicoes_Analitico_SC_202111_Desonerado.csv'
+file_insumos_ficha = 'Arquivos A a Z.csv'
+file_insumos_ficha_marcas = 'Arquivos A a Z_com_marcas.csv'
 
 df = pd.read_csv(file)
-df[['CODIGO ITEM', 'CODIGO DA COMPOSICAO']] = df[['CODIGO ITEM', 'CODIGO DA COMPOSICAO']].fillna(0).astype(dtype = int)#, errors = 'ignore')
+df[['CODIGO ITEM', 'CODIGO DA COMPOSICAO']] = df[['CODIGO ITEM',
+                                                  'CODIGO DA COMPOSICAO']].fillna(0)\
+                                                    .astype(dtype = int)#, errors = 'ignore')
 
-df_insumos = df.loc[df['TIPO ITEM']=='INSUMO',['TIPO ITEM','CODIGO ITEM', 'DESCRIÇÃO ITEM','UNIDADE ITEM','PRECO UNITARIO']].drop_duplicates()#,'CUSTO TOTAL']]
-df_composicao = df[['CODIGO DA COMPOSICAO','DESCRICAO DA COMPOSICAO','UNIDADE','CUSTO TOTAL']].drop_duplicates().copy()
+df_ficha = pd.read_csv(file_insumos_ficha)
+df_ficha_marcas = pd.read_csv(file_insumos_ficha_marcas)
+
+df_insumos = df.loc[df['TIPO ITEM']=='INSUMO',
+                    ['TIPO ITEM','CODIGO ITEM', 'DESCRIÇÃO ITEM',
+                     'UNIDADE ITEM','PRECO UNITARIO']]\
+                        .drop_duplicates()#,'CUSTO TOTAL']]
+
+df_composicao = df[['CODIGO DA COMPOSICAO','DESCRICAO DA COMPOSICAO',
+                    'UNIDADE','CUSTO TOTAL']].drop_duplicates().copy()
 
 
 
-st.title('SINAPI Outubro 2021 - Desonerado')
+st.title('SINAPI Novembro 2021 - Desonerado')
 
-lista_opcoes = ['insumo', 'composicao', 'insumos_composicao']
+lista_opcoes = ['insumo', 'composicao', 'insumos_composicao', 'ficha_especificacao', 'ficha_especificacao_com_marcas']
 tipo_busca = st.radio('Tipo de busca', lista_opcoes,0)
 palavras = st.text_input('Busca por palavras ou número de composição se "insumos_composicao"')
 palavras = unicodedata.normalize('NFKD', palavras).encode('ascii','ignore').decode("utf-8") 
@@ -31,23 +43,33 @@ def busca_sinapi(palavras, tipo_busca):
         df_consulta = df_composicao.copy()
         coluna = 'DESCRICAO DA COMPOSICAO'
     elif tipo_busca==lista_opcoes[2]:
-        df_consulta = df[['CODIGO DA COMPOSICAO','DESCRICAO DA COMPOSICAO','TIPO ITEM','CODIGO ITEM', 'DESCRIÇÃO ITEM','UNIDADE ITEM','PRECO UNITARIO']].copy()
+        df_consulta = df[['CODIGO DA COMPOSICAO','DESCRICAO DA COMPOSICAO',
+                          'TIPO ITEM','CODIGO ITEM', 'DESCRIÇÃO ITEM',
+                          'UNIDADE ITEM','PRECO UNITARIO']].copy()
+
         coluna = 'CODIGO DA COMPOSICAO'
+    elif tipo_busca==lista_opcoes[3]:
+        df_consulta = df_ficha[df_ficha.columns[1:]]
+        coluna='Descrição Básica'
+    elif tipo_busca==lista_opcoes[4]:
+        df_consulta = df_ficha_marcas[df_ficha_marcas.columns[1:]]
+        coluna='Referencial / Parâmetro de Pesquisa'
      
     for palavra in palavras.split(' '):
         if tipo_busca!=lista_opcoes[2]:
-            df_consulta = df_consulta[df_consulta[coluna].astype(str).str.lower().str.normalize('NFKD').str.encode('ascii', errors='ignore').str.decode('utf-8').str.contains(palavra.lower())].copy()
+            df_consulta = df_consulta[df_consulta[coluna]\
+                .astype(str).str.lower()\
+                .str.normalize('NFKD')\
+                .str.encode('ascii', errors='ignore')\
+                .str.decode('utf-8')\
+                .str.contains(palavra.lower())].copy()
         else:
-            try :
+            
 
-
-                st.write(palavra)             
-                st.write(df_consulta.loc[df_consulta[coluna]==int(palavras), 'DESCRICAO DA COMPOSICAO'].tolist()[0])
-                df_consulta =df_consulta.loc[df_consulta[coluna]==int(palavras), df_consulta.columns[2:]].dropna()
-
-            except:
-                st.write("Tente um número de composição existente")
-
+            st.write(palavra)             
+            st.write(df_consulta.loc[df_consulta[coluna]==int(palavras), 'DESCRICAO DA COMPOSICAO'].tolist()[0])
+            df_consulta =df_consulta.loc[df_consulta[coluna]==int(palavras), df_consulta.columns[2:]].dropna()
+    
     
     return df_consulta
 
